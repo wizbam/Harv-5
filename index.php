9d323a18af414247ff0381d447d5cdb441dc96ac
wizbam <johnddaniels@gmail.com>
2013-03-21 10:01:14 -0300

    Adding commands and notes.
    Added SQL connection, a couple commands, and notes to all the functions

--- index.php
+++ index.php
@@ -1,56 +1,75 @@
 <?php
+
+// Connect to local SQL database
+$con = mysqli_connect("localhost","root","","harv-5");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
  
+// Prevent PHP from stopping the script after 30 sec
 set_time_limit(0);
  
+// Opening the socket
 $socket = fsockopen("irc.freenode.net", 6667);
-
-$steamid = array( "Harv-5" => "Wizbam" );
  
-fputs($socket,"USER Harv-5 arcanetheory.net Harv-5 : Harv-5\n");
+$steamlookup = mysqli_query($con,"SELECT * FROM steamid ");
+$nick = strpos($ex[0], '!');
+
+	while($row = mysqli_fetch_array($steamlookup))
+  {
+	echo $row['Nick'] . " " . $row['SteamID'];
+  }
+
+ 
+// Send auth info
+fputs($socket,"USER Harv-5 arcanetheory.net Harv-5 :Harv-5\n");
 fputs($socket,"NICK Harv-5\n");
  
+// Join channel
 fputs($socket,"JOIN #digitalgames\n");
  
+// Force endless while
 while(1) {
  
+	// Create variable for data received from IRC
 	while($data = fgets($socket, 128)) {
  
 		echo nl2br($data);
 		flush();
  
+		// Separate all data
 		$ex = explode(' ', $data);
  
+		// Send PONG back to the server
 		if($ex[0] == "PING"){
 			fputs($socket, "PONG ".$ex[1]."\n");
 		}
  
+		// Commands with nothing after them
 		$command = str_replace(array(chr(10), chr(13)), '', $ex[3]);
+		// Commands with something after them
 		$command2 = str_replace(array(chr(10)), '', $ex[3]);
+		
 		if ($command == ":!report") {
-			fputs($socket, "PRIVMSG ".$ex[2]." :Harv-5 Reporting for duty!\n");
-		}
-		if ($command == ":!turntable") {
-			fputs($socket, "PRIVMSG ".$ex[2]." :TurnTable.fm: http://goo.gl/t0ck8 \n");
+			fputs($socket, "PRIVMSG ".$ex[2]." :Harv-5 reporting for duty!\n");
 		}
 		
-		$read = file_get_contents('http://dl.dropbox.com/u/10458699/ign.txt');
-		
-		if ($command == ":!read") {
-			fputs($socket, "PRIVMSG ".$ex[2]." $read \n");
-		}
-		
-		if ($command == ":!steam") {
-			fputs($socket, "PRIVMSG ".$ex[2]." :I can't...exactly do that yet.\n");
+		if ($command == ":fuck") {
+			fputs($socket, "PRIVMSG ".$ex[2]." :No! Fuck you!\n");
 		}
 		
 		if ($command2 == ":!link") {
-			fputs($socket, "PRIVMSG ".$ex[2]." :Welllll, sorry folks. Still workin' on that function.\n");
+			fputs($socket, "PRIVMSG ".$ex[2]." :Steam ID linked to $ex[0]\n");
+			mysqli_query($con,"INSERT INTO steamid (Nick, SteamID) VALUES ('$nick','$ex[4]')");
 		}
 		
 		if ($command2 == ":!steam") {
-			fputs($socket, "PRIVMSG ".$ex[2]." :Yeeeaah. I'm not ready to do that one yet.\n");
+			fputs($socket, "PRIVMSG ".$ex[2]." :$steamlookup\n");
 		}
- 
+		
 	}
  
 }
